31fca95167bdc58b057fb08d9640c4e86cf635b2
jrojo66 <javier.rojo66@gmail.com>
2025-06-07 16:30:34 -0300

    al 7.Jung

--- JS/iniciarSesion.js
+++ JS/iniciarSesion.js
@@ -219,7 +219,7 @@ document
       })
       .catch((error) => {
         console.error("Error en reset de contraseña:", error);
-        showConfirmOkOnly(`Error al cambiar la contraseña ${error}`, error)
+        showConfirmOkOnly("❌ No se pudo enviar el email. Por favor, intentá más tarde.");
       })
       .finally(() => {
         spinner.style.display = "none"; //spinner
