dc14be41a5a8e02b36a11bfe084fdb7840260d94
jrojo66 <javier.rojo66@gmail.com>
2025-06-16 16:39:06 -0300

    Al 16.Junf

--- JS/config.js
+++ JS/config.js
@@ -1,3 +1,19 @@
 // const ROOT_URL = "http://localhost:8080"; // Para desarrollo
 // const ROOT_URL = "https://beepsafe-10-0-back-production.up.railway.app"; // Para produccion railway
 const ROOT_URL = "https://beepsafe-p11h.onrender.com" // Para produccion
+
+
+// const FRONT_URL = "http://localhost:57621";
+// const FRONT_URL = "https://jrojo66.github.io/Beepsafe-1.0.0-front/";
+
+
+// const isLocalhost = location.hostname === "localhost";
+
+// const ROOT_URL = isLocalhost
+//   ? "http://localhost:8080"
+//   : "https://beepsafe-p11h.onrender.com";
+
+// const FRONT_URL = isLocalhost
+//   ? `http://${location.hostname}:${location.port}`
+//   : "https://jrojo66.github.io/Beepsafe-1.0.0-front";
+
